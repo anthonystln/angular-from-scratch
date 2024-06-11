08a2a3e5cad10bf92fa94efd14b0e1d09140b7c1
anthonystln <austin619@outlook.fr>
2024-06-11 13:20:40 +0200

    Code de fin de la selection Decouverte des directives

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import { CreditCardDirective } from "./directives/credit-card.directive";
+import { PhoneNumberDirective } from "./directives/phone-number.directive";
+
+// Framework
+const directives = [PhoneNumberDirective, CreditCardDirective];
+
+directives.forEach(directive => {
+	const elements = document.querySelectorAll<HTMLElement>(directive.selector);
+
+	elements.forEach(element => {
+		const directiveInstance = new directive(element);
+		directiveInstance.init();
+	});
+});
